3efcd04cd9b3fadd2f4eaaec021244068d0a74bd
Bartosz Łaniewski <laniewski.bartozzz@gmail.com>
2018-06-08 18:03:39 +0200

    Update About us section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -13,7 +13,32 @@ class AboutScreen extends React.Component {
 
           <CardItem>
             <Content>
-              <Text>Goo</Text>
+              <Text>
+                We’ve created this application, because we believe that nobody
+                should be allowed to burn garbage in their houses – that’s
+                dangerous for the environment and everybody’s health.
+                {'\n'}
+              </Text>
+
+              <Text>
+                Burning plastic, rubber or other man-made materials creates
+                dioxin and other dangerous toxics in the air, soil &
+                groundwater.
+                {'\n'}
+              </Text>
+
+              <Text>
+                This endangers you, your children, your neighbours, your pets,
+                birds and fish – with tumours, cancer, learning disorders,
+                infertility, immune system problems, asthma, and other diseases.
+                {'\n'}
+              </Text>
+
+              <Text>
+                Children, teenagers & pregnant women are at the highest
+                risk. The resulting ash is also toxic and can easily get into
+                the ground and your well water.
+              </Text>
             </Content>
           </CardItem>
         </Card>
